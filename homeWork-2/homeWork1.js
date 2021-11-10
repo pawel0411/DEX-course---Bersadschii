8b4aaec80622763fe1100db875577594953019ff
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-10 15:53:32 +0200

    Update homeWork1.js
     Должны складываться только строки и числа
      // Постарайтесь сложить как можно больше пар и обойти NaN случаи

--- homeWork-2/homeWork1.js
+++ homeWork-2/homeWork1.js
@@ -1,1 +1,19 @@
+ if ((typeof(f)=='number'||typeof(f)=='string')&&
+     (typeof(s)=='string'||typeof(s)=='number'))
+     if ((parseFloat(f)===NaN)&&(parseFloat(s)===NaN))
+     return 0;
+     if()
 
+     // let stF=+f[0];
+     // let stS=+s[0];
+      //let cutF= f.substring().slice(1,f.substring().length);
+      //let cutS= s.substring().slice(1,s.substring().length);
+      
+    // if ((typeof(stF)==NaN)&&(typeof(stS)==NaN))
+    // return parseFloat(cutF)+parseFloat(cutS);
+     //if ((typeof(stF)!=NaN)&&(typeof(stS)==NaN))
+    // return parseFloat(f)+parseFloat(cutS);
+    // if ((typeof(stF)==NaN)&&(typeof(stS)!=NaN))
+    // return parseFloat(cutF)+parseFloat(s);
+     return parseFloat(f)+parseFloat(s);
+      
