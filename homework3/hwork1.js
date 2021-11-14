d09baa0feb7bb32cf0e6e5ba833d75ac5a5d8572
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-14 20:47:27 +0200

    Update hwork1.js
    //TODO: Нужно добавить поле discount(oldPrice - price)
      // убрать поле oldPrice

--- homework3/hwork1.js
+++ homework3/hwork1.js
@@ -1,9 +1,13 @@
  //TODO: Нужно добавить поле discount(oldPrice - price)
   // убрать поле oldPrice
-  for (let fruit of cartData) {
-     if((fruit.oldPrice-fruit.price)>0)
-     fruit.discount=fruit.oldPrice-fruit.price;
-  } 
+ for (let fruit of cartData) {
+  if((fruit.oldPrice-fruit.price)>0)
+  fruit.discount=fruit.oldPrice-fruit.price;
+} 
 for (let fruit of cartData) 
-    delete fruit.oldPrice;
-  return cartData;
+ delete fruit.oldPrice;
+ for (let i=0; i<cartData.length; i++) {
+  let name=cartData[i];
+  console.log( `${name.id} ${name.type}${name.price}
+  ${name.name}${name.count} ${name.date} ${name.discount}`); 
+ }
