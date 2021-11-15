9b1d9302694e7aac84f054744ecf737bbabdf57d
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-15 19:46:51 +0200

    Create funcExpr3.js
    Максимальная зарплата

---
+++ independentWork-3/funcExpr3.js
@@ -0,0 +1,18 @@
+let salaries = {
+  "John": 100,
+  "Pete": 300,
+  "Mary": 250
+};
+function topSalary(salaries){
+  let max=0;
+  let maxName=null; 
+  for (let [key, value] of Object.entries(salaries)) {
+       if (max<value) {
+         max=value;
+         maxName=key;
+       }  
+  }
+  return`${maxName}`; 
+}
+console.log(topSalary(salaries));
+//Максимальная зарплата
