88e7c7c417c63d77daf7678815186aecfca0555e
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-15 13:57:49 +0200

    Update hwork2.js

--- homework3/hwork2.js
+++ homework3/hwork2.js
@@ -9,5 +9,9 @@
   fruits.addedIngredients[0]=fruits1;
 
 console.log(fruits); 
+
+cartItem[cartItem.length]=fruits;//если надо добавить копию в массив
+
+  return cartItem;
  //TODO: сделать копию элемента "Пицца с анчоусами"
   // После увеличить кол-во добавленного ингредиента
