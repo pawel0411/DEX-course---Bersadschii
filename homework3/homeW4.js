d9c2c1a032db6ec70e3e1abfa2513223721460d6
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-15 10:53:58 +0200

    Update homeW4.js

--- homework3/homeW4.js
+++ homework3/homeW4.js
@@ -1,3 +1,7 @@
 //TODO: выбрать покупки сделанные за выбранную дату
 
-  return cartData.filter(item => item.date == date);
+//TODO: выбрать покупки сделанные за выбранную дату
+  let mass= cartData.filter(item => item.date == date);
+  if (mass.length>0)
+  return mass;
+  return cartData;
