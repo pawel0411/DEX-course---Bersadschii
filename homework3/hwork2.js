1c3234297ce0dffcd549813b955b263a570cecc4
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-15 13:25:43 +0200

    Update hwork2.js

--- homework3/hwork2.js
+++ homework3/hwork2.js
@@ -3,23 +3,11 @@
   { if (fruit.name==="Пицца с анчоусами")
       fruits = Object.assign({}, fruit);
   }
-  fruits.addedIngredients.count++;
-  console.log( fruits); 
-  console.log(fruits.addedIngredients.count);
-//TODO: сделать копию элемента "Пицца с анчоусами"
+  let fruits1 = Object.assign({}, fruits.addedIngredients[0]);
+  delete fruits.addedIngredients[0];
+  fruits1.count++
+  fruits.addedIngredients[0]=fruits1;
+
+console.log(fruits); 
+ //TODO: сделать копию элемента "Пицца с анчоусами"
   // После увеличить кол-во добавленного ингредиента
-
-
-------
-let nameProd="Пицца с анчоусами";
-let fruits= cartData.find(item => item.name === nameProd);
-------
-let fruits;
-  for (let fruit of cartData)
-  { if (fruit.name==="Пицца с анчоусами")
-      fruits = Object.assign({}, fruit);
-  }
-let doublePrices = Object.fromEntries(
-    Object.entries(fruits.addedIngredients).map(([key, value]) => (key== 'count')?[key, value]: [key, value]));
- 
-    console.log(doublePrices);
