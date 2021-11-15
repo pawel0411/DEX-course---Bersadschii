615ae2b8c45bc2e6ce11798da174e057ab8bd5d3
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-15 11:01:36 +0200

    Update homeW5.js

--- homework3/homeW5.js
+++ homework3/homeW5.js
@@ -1,11 +1,7 @@
-let date ="2021-10-30T20:55:15.220Z";
 let now = new Date();
-
   let arr1=cartData.filter(item => item.date === date);
-  
   let arr =[];
 
-  
   for (let j=0; j<arr1.length; j++)
   {
     arr[j]=Object.assign({}, arr1[j]);
@@ -15,24 +11,18 @@ let now = new Date();
  
   for (let i of arr)
   {
-  while (true)
-  {
+  while (true) {
       let id =Math.floor(Math.random() * 100);
       let index= cartData.findIndex(item=> item.id===id)
       if (index===-1)
       {
         i.id=id;
          break;
-      }
-  
+      }  
   }
 }
 let mass = arr.concat(cartData);
-for (let i=0; i<mass.length; i++) {
-  let name=mass[i];
-  console.log( `${name.id} ${name.type}${name.price}
-  ${name.name}${name.count} ${name.date}`); 
- }
+return mass;
   //TODO: нужно повторить заказ за выбранную дату
   // дублиовать соответствующие элементы
   // поставить в начало спиcка
