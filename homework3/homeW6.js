ff360561ec9d7188c43a010b41b83961526943ab
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-15 13:45:32 +0200

    Update homeW6.js

--- homework3/homeW6.js
+++ homework3/homeW6.js
@@ -1,6 +1,6 @@
 let name ="Сок яблочный";
 let someUsers = cartData.filter(item => item.name ===name);
-//console.log(someUsers.length);
+
 for (let i=0;i<someUsers.length; i++)
 someUsers[i].count++;
   //TODO: увеличить кол-во товара в полученном элементе
