23d2d4b0d038b04de9427245c9721629e4c991a6
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-15 16:26:22 +0200

    Update function3.js

--- independentWork-3/function3.js
+++ independentWork-3/function3.js
@@ -6,3 +6,18 @@ function pow (a,b)
   return a**b;
 }
 //Функция pow(x,n)
+--------
+let a=3;
+let b=4;
+if (b>0){
+  console.log(pow(a,b));
+}
+else{
+  console.log("число b не натурально");
+}
+function pow (a,b)
+{let result=a;
+        for (let i=1;i<b;i++)
+       result*=a;
+      return result;
+}
