cee707ffc56dd2e19452e9468c2487a506247055
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-22 11:35:34 +0200

    Update 4.js

--- homeWork-5/4.js
+++ homeWork-5/4.js
@@ -1,30 +1,22 @@
 const getDataFromAPI = async () => {
-  async function f() {
+  try
+  {
+    let response = await fetch("https://randomuser.me/api123");
+    
+       if (response.ok) { 
 
-    try {
-      let response = await fetch("https://randomuser.me/api");
-    } catch(err) {
-      (err); // TypeError: failed to fetch
-    }
-  }
-  
-  f();
+  let json = await response.json();
+        console.log("все работает"+ json);
+          } else {
+            throw new Error("Ошибка HTTP: " + response.status);           
+          }
+        
+      }
+      catch(err)
+      {
+        console.log(err+" Ошибка!");
+      }
 };
----------------------
-  // Написать функцию, которая принимает url и выводит в консоль ответ
+// Написать функцию, которая принимает url и выводит в консоль ответ
 // TODO: обработать ошибки и вывести в консоль "Ошибка"
 // TODO: ошибка если тстатус меньше 200 и больше 299
-const getDataFromAPI = async () => {
-  let response = await fetch("https://randomuser.me/api");
-
-if (response.ok) { // если HTTP-статус в диапазоне 200-299
-  // получаем тело ответа (см. про этот метод ниже)
-  let json = await response.json();
-} else {
-  console.log("Ошибка HTTP: " + response.status);
-}
-};
-
-getDataFromAPI("https://randomuser.me/api");
-getDataFromAPI("https://randomuser123.me/api");
-getDataFromAPI("https://randomuser.me/api123");
