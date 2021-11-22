c4a305f30af7bed9a02315294679866c2caeefdf
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-22 10:16:15 +0200

    Update work1.js

--- independentWork-4/classWork-4/work1.js
+++ independentWork-4/classWork-4/work1.js
@@ -5,3 +5,18 @@ return fetch (url.name=='Oleg').then((r)=>r.json());
 };
 getUsers("GET", requestURL).then((data)=> console.log(data));
 -------------------------
+
+  //решение работает
+  
+  (async () => {
+  let url = "https://jsonplaceholder.typicode.com/users";
+  let response = await fetch(url);
+  
+  let commits = await response.json(); // читаем ответ в формате JSON
+  for (let i=0; i<commits.length;i++)
+  {
+
+    if (commits[i].name=="Leanne Graham")
+    console.log (commits[i]);  
+  }
+  })()
