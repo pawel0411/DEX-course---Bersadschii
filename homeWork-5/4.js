9152ed52e6baa2a113dafff11c876774d16cc7c7
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-22 11:01:10 +0200

    Update 4.js

--- homeWork-5/4.js
+++ homeWork-5/4.js
@@ -10,3 +10,21 @@ const getDataFromAPI = async () => {
   
   f();
 };
+---------------------
+  // Написать функцию, которая принимает url и выводит в консоль ответ
+// TODO: обработать ошибки и вывести в консоль "Ошибка"
+// TODO: ошибка если тстатус меньше 200 и больше 299
+const getDataFromAPI = async () => {
+  let response = await fetch("https://randomuser.me/api");
+
+if (response.ok) { // если HTTP-статус в диапазоне 200-299
+  // получаем тело ответа (см. про этот метод ниже)
+  let json = await response.json();
+} else {
+  console.log("Ошибка HTTP: " + response.status);
+}
+};
+
+getDataFromAPI("https://randomuser.me/api");
+getDataFromAPI("https://randomuser123.me/api");
+getDataFromAPI("https://randomuser.me/api123");
