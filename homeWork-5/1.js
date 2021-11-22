d3f6b04cf3e6af74199c868e5b9062c582a3f32c
Павел Бершадский <93705697+pawel0411@users.noreply.github.com>
2021-11-22 10:58:57 +0200

    Update 1.js

--- homeWork-5/1.js
+++ homeWork-5/1.js
@@ -2,8 +2,8 @@ import { compareText } from "./data";
 const answer =(compareText)=>
 {
   if (compareText.then)
-  compareText.then (console.log("выполнено успешно"));
-   compareText.catch (console.log("выполнено ошибочно"));
+  compareText.then (return("выполнено успешно"));
+   compareText.catch (return("выполнено ошибочно"));
 }
 // первая обработает ответ от compareText (положительный и отрицательный)
 // TODO: первая функция использует then и catch
